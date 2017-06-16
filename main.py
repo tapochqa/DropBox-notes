d13f8f6b372793110bfaeb5084bc36e7572215dd
Suckovich <iwill@nottellyou.myemail>
2017-06-16 13:53:23 +0300

    Мы проверили, как работает API
    ВСЁ ПРОШЛО ПРОСТО ЗАМЕЧАТЕЛЬНО!

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import dropbox
+from dropbox.files import WriteMode
+import sys
+
 
 with open ('token', 'r') as token:
-    oauth_token = token.readlines()
+    oauth_token = token.readline()
 
-print oauth_token
+dbx = dropbox.Dropbox(oauth_token)
+
+with open ('check.txt', 'rb') as ofile:
+    dbx.files_upload(ofile.read(), '/check.txt', mode = WriteMode('overwrite'))
